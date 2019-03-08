2750137017bf6e2dbae0b52660849a18a631410b
ludeeus <joasoe@gmail.com>
2019-03-08 22:41:38 +0100

    Fix BinarySensor Class name

--- custom_components/blueprint/binary_sensor.py
+++ custom_components/blueprint/binary_sensor.py
@@ -9,10 +9,10 @@ async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None
 ):  # pylint: disable=unused-argument
     """Setup sensor platform."""
-    async_add_entities([BlueprintSensor(hass)], True)
+    async_add_entities([BlueprintBinarySensor(hass)], True)
 
 
-class BlueprintSensor(BinarySensorDevice):
+class BlueprintBinarySensor(BinarySensorDevice):
     """blueprint Sensor class."""
 
     def __init__(self, hass):
