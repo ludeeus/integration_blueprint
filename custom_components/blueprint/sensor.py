04f877251791d3d36040162a29b177d99a9b16bf
oncleben31 <oncleben31@users.noreply.github.com>
2020-11-11 01:09:47 +0100

    Fix missing import for slugify

--- custom_components/blueprint/sensor.py
+++ custom_components/blueprint/sensor.py
@@ -2,6 +2,8 @@
 from custom_components.blueprint.const import DEFAULT_NAME, DOMAIN, ICON, SENSOR
 from custom_components.blueprint.entity import BlueprintEntity
 
+from homeassistant.util import slugify
+
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
