8600eccc7297fe6e5640335395f72092af114d9c
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2021-05-28 11:16:14 -0400

    typo

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 
 
 # This fixture enables loading custom integrations in all tests.
-# Remove to enable selective uae of this fixture
+# Remove to enable selective use of this fixture
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     yield
