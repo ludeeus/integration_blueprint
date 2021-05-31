cb91d2f547c04b3251bfc0c2e05d9c500b4af75d
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2021-05-31 03:33:44 -0400

    Use `enable_custom_integrations` fixture by default (#58)

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,6 +21,13 @@ import pytest
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 
+# This fixture enables loading custom integrations in all tests.
+# Remove to enable selective use of this fixture
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    yield
+
+
 # This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
 # notifications. These calls would fail without this fixture since the persistent_notification
 # integration is never loaded during a test.
