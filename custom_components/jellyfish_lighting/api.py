57e8255fa5b98701fdf3fc472b2e70a29bd5242d
Bryson Dunn <k.bryson.dunn@gmail.com>
2022-12-23 19:45:43 +0000

    Add zone support

--- custom_components/jellyfish_lighting/api.py
+++ custom_components/jellyfish_lighting/api.py
@@ -16,26 +16,59 @@ class JellyfishLightingApiClient:
         self, host: str, session: aiohttp.ClientSession, hass: HomeAssistant
     ) -> None:
         """Initialize API client."""
-        self._host = host
+        self.host = host
         self._session = session
         self._hass = hass
-        self._controller = jf.JellyFishController(self._host, True)
+        self._controller = jf.JellyFishController(host, False)
+        self.zones = None
+        self.patterns = None
 
     async def async_get_data(self):
         """Get data from the API."""
         try:
             # TODO: extend JF library to retrieve zone states
             self._controller.connectAndGetData()
+            self.zones = self._controller.zones
+            self.patterns = [p.toFolderAndName() for p in self._controller.patternFiles]
+            self.patterns.sort()
+            _LOGGER.debug("Zones:\n%s", " ".join(self.zones))
+            _LOGGER.debug("Patterns:\n%s", " ".join(self.patterns))
         except BaseException as ex:  # pylint: disable=broad-except
             _LOGGER.exception(
-                "Failed to connect to Jellyfish Lighting controller at %s", self._host
+                "Failed to connect to Jellyfish Lighting controller at %s", self.host
             )
             raise ex
 
     async def async_turn_on(self, zones: List[str] = None):
         """Turn one or more zones on. Affects all zones if zone list is None"""
-        self._controller.turnOn(zones)
+        try:
+            self._controller.turnOn(zones)
+        except BaseException as ex:  # pylint: disable=broad-except
+            _LOGGER.exception(
+                "Failed to connect to turn on Jellyfish Lighting zone(s) '%s'",
+                zones or "[all zones]",
+            )
+            raise ex
 
     async def async_turn_off(self, zones: List[str] = None):
         """Turn one or more zones off. Affects all zones if zone list is None"""
-        self._controller.turnOff(zones)
+        try:
+            self._controller.turnOff(zones)
+        except BaseException as ex:  # pylint: disable=broad-except
+            _LOGGER.exception(
+                "Failed to connect to turn of Jellyfish Lighting zone(s) '%s'",
+                zones or "[all zones]",
+            )
+            raise ex
+
+    async def async_play_pattern(self, pattern: str, zones: List[str] = None):
+        """Turn one or more zones off. Affects all zones if zone list is None"""
+        try:
+            self._controller.playPattern(pattern, zones)
+        except BaseException as ex:  # pylint: disable=broad-except
+            _LOGGER.exception(
+                "Failed to play pattern '%s' on Jellyfish Lighting zone(s) '%s'",
+                pattern,
+                zones or "[all zones]",
+            )
+            raise ex
--- custom_components/jellyfish_lighting/light.py
+++ custom_components/jellyfish_lighting/light.py
@@ -1,53 +1,63 @@
 """Switch platform for jellyfish-lighting."""
-from homeassistant.components.light import LightEntity
-
+import re
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.components.light import LightEntity, LightEntityFeature, ColorMode
 from .const import DOMAIN
 from . import JellyfishLightingDataUpdateCoordinator
 from .entity import JellyfishLightingEntity
 
-_ICON = "mdi:led-strip-variant"
+_ALL_ZONES = "All Zones"
 
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup light platform"""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    # TODO: Add light per zone, and one to control them all
-    async_add_devices([JellyfishLightingLight(coordinator, entry)])
+    lights = [
+        JellyfishLightingLight(coordinator, entry, zone)
+        for zone in coordinator.api.zones
+    ]
+    if len(lights) > 1:
+        lights.insert(0, JellyfishLightingLight(coordinator, entry, _ALL_ZONES))
+    async_add_devices(lights)
 
 
 class JellyfishLightingLight(JellyfishLightingEntity, LightEntity):
     """jellyfish-lighting light class."""
 
     def __init__(
-        self, coordinator: JellyfishLightingDataUpdateCoordinator, entity: LightEntity
+        self,
+        coordinator: JellyfishLightingDataUpdateCoordinator,
+        entry: ConfigEntry,
+        zone: str,
     ) -> None:
         """Initialize."""
-        self._on = False
-        super().__init__(coordinator, entity)
+        self._attr_supported_features = LightEntityFeature.EFFECT
+        self._attr_supported_color_modes = ColorMode.ONOFF
+        self._attr_color_mode = ColorMode.ONOFF
+        self._attr_icon = "mdi:led-strip-variant"
+        self._attr_assumed_state = True  # TODO: Remove once polling is added
+        self.zone = zone
+        self.api_zone = None if self.zone == _ALL_ZONES else [self.zone]
+        self.uid = re.sub("[^a-z0-9]", "_", zone.lower().strip("_"))
+        self._attr_has_entity_name = True
+        self._attr_name = zone
+        self._attr_is_on = False
+        self._attr_effect = None
+        self._attr_effect_list = coordinator.api.patterns
+        super().__init__(coordinator, entry)
+
+    @property
+    def unique_id(self):
+        return self.uid
 
     async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
         """Turn on the light."""
-        await self.coordinator.api.async_turn_on()
-        self._on = True
-        await self.coordinator.async_request_refresh()
+        await self.coordinator.api.async_turn_on(self.api_zone)
+        self._attr_is_on = True
+        # await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
         """Turn off the light."""
-        await self.coordinator.api.async_turn_off()
-        self._on = False
-        await self.coordinator.async_request_refresh()
-
-    @property
-    def name(self):
-        """Return the name of the light."""
-        return f"{DOMAIN}_all_zones"
-
-    @property
-    def icon(self):
-        """Return the icon of this switch."""
-        return _ICON
-
-    @property
-    def is_on(self):
-        """Return true if the switch is on."""
-        return self._on
+        await self.coordinator.api.async_turn_off(self.api_zone)
+        self._attr_is_on = False
+        # await self.coordinator.async_request_refresh()
