f4872ac82bfc337d25f92abe4f5588e448c8cae4
Matt Colyer <matt@colyer.name>
2024-01-22 13:49:58 -0800

    test_sensor is green

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -10,14 +10,19 @@ from custom_components.anova_nano.const import (
 
 from .const import MOCK_CONFIG
 
-async def test_sensor(hass):
+async def test_sensor(hass, aioclient_mock):
     """Test sensor."""
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG)
     entry.add_to_hass(hass)
+
+    aioclient_mock.get(
+        "https://jsonplaceholder.typicode.com/posts/1", text='{"body": "value"}'
+    )
+
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     state = hass.states.get("sensor.integration_sensor")
 
     assert state
-    assert state.state == "23"
+    assert state.state == "value"
