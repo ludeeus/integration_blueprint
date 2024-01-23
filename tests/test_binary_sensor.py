98fe82fdddb8bd29091f4c2af5866e4c33c3d664
Matt Colyer <matt@colyer.name>
2024-01-23 14:18:48 -0800

    Add a test for the binary sensor platform

---
+++ tests/test_binary_sensor.py
@@ -0,0 +1,25 @@
+"""Test binary sensor for Anova Nano."""
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.anova_nano.const import DOMAIN
+
+from .const import MOCK_CONFIG
+
+
+async def test_binary_sensor(hass, aioclient_mock):
+    """Test sensor."""
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG)
+    entry.add_to_hass(hass)
+
+    aioclient_mock.get(
+        "https://jsonplaceholder.typicode.com/posts/1", text='{"body": "value"}'
+    )
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("binary_sensor.anova_nano_binary_sensor")
+
+    assert state
+    assert state.state == "off"
