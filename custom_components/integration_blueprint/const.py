5366fa5d9877502906e50116c0b62f8e879ca272
ludeeus <joasoe@gmail.com>
2024-06-08 19:35:18 +0000

    General updates June 2024

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,9 +1,10 @@
 """Constants for integration_blueprint."""
+
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
 NAME = "Integration blueprint"
 DOMAIN = "integration_blueprint"
-VERSION = "0.0.0"
+VERSION = "1.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
